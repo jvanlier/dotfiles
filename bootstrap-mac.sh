f1fe49d65842d5a04b689349d434cc1b4c46ae37
Jori van Lier <jori@jvlanalytics.nl>
2020-11-24 21:21:06 +0100

    Add powerlevel10k, vim

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -13,12 +13,22 @@ brew install pyenv pyenv-virtualenv
 # - pyenv global base
 # - install pipx
 
+
 echo "Installing oh my zsh..."
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-
+# TODO: install Meslo Nerd Font
+# https://github.com/romkatv/powerlevel10k#oh-my-zsh
 
 ln -s ./.zshrc ~/.zshrc
 ln -s ./.p10k.zsh ~/.p10k.zsh
+
+
+echo "Configuring vim..."
 ln -s ./.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+python setup.py
