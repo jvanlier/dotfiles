757e77833281feb01c8d46cda154cb5e107bcd54
Jori van Lier <jori.vanlier.ext@adevinta.com>
2022-06-08 12:53:01 +0200

    MacOS Moneterey updates

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env /bin/bash
 set -eu
 
-PY3_VERSION="3.8.6"
+PY3_VERSION="3.10.4"
 BASE_VENV="base-${PY3_VERSION}"
 TS=$(date +'%Y-%m-%dT%H-%M-%S')
 
@@ -16,28 +16,20 @@ else
 fi
 
 PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install -s $PY3_VERSION
-set +e
-set +x
-pyenv virtualenvs|grep ${PY3_VERSION}/envs/${BASE_VENV}
-set -e
-if [ $? -ne 0 ]; then
-    pyenv virtualenv $PY3_VERSION ${BASE_VENV}
-    pyenv global ${BASE_VENV}
-    pip install --upgrade pip
-    pip install pipx
-else
-    echo "virtualenv ${BASE_VENV} already installed, skipping"
-fi
+pyenv global $PY3_VERSION
+pip install --upgrade pip
+pip install --upgrade pipx
 
 # Zsh
-echo "Checking for oh my zsh..."
+echo -e "\nChecking for oh my zsh..."
 
 if [ ! -d ${HOME}/.oh-my-zsh ]; then
-    echo "Could not find oh-my-zsh dir. Will chsh to zsh first and then install oh-my-zsh."
-    chsh -s /bin/zsh
-    echo "Installing oh my zsh..."
-    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "Could not find oh-my-zsh dir. Installing..."
+    export CHSH=no  # chsh is no longer needed since MacOS Catalina
+    export RUNZSH=no  # by default, it runs zsh directly and this script does not continue 
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Installing oh-my-zsh plugins..."
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 else
     echo "oh my zsh already installed, skipping"
@@ -52,16 +44,18 @@ cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 
 # Vim
-echo "Configuring vim..."
+echo -e "\nConfiguring vim..."
 
 if [ ! -d ${HOME}/.vim/bundle ]; then
+    brew install vim  # default vim has no Python3 support, brew verson does
+    source ~/.zshrc  # reload PATH to pick up brew version
     echo "Installing Vundle with YouCompleteMe"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
-    OLD_PWD=$(pwd)
-    cd ~/.vim/bundle/YouCompleteMe
-    python setup.py
-    cd $OLD_PWD
+    brew install cmake  # needed to compile YCM
+    pushd ${HOME}/.vim/bundle/YouCompleteMe
+    python install.py
+    popd
 else
     echo "Vundle already installed, skipping installation of Vundle and YouCompleteMe"
 fi
@@ -75,4 +69,8 @@ fi
 cp .vimrc ~/.vimrc
 cp .ideavimrc ~/.ideavimrc
 
-echo "All done! If this is the first time installing powerline10k, run 'p10k configure' to install Meslo Nerd font."
+echo -e "\nAll done! Running zsh now."
+echo "If this is the first time installing powerline10k, run 'p10k configure' to install Meslo Nerd font."
+
+zsh
+
