64babd74a13959e2636ceaf981d5d86a047d3547
Jori <jori@nomail>
2026-06-11 11:14:34 +0200

    pin nvim-treesitter to v0.10.0 to avoid incompatible v1 rewrite
    v1 (HEAD of master) dropped the configs module and requires tree-sitter CLI
    binary which is not available. v0.10.0 is the last stable tag with the old API.

--- dotfiles/config/nvim/lua/config/plugins/treesitter.lua
+++ dotfiles/config/nvim/lua/config/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
+  tag = "v0.10.0",
+  event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
